3703d448bbb3ec5a91e82ea75a5dc4ce4b866d5f
RafsanRifat <rafsan.rifat255@gmail.com>
2022-05-17 12:19:45 +0600

    list rendering complete

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-
-
 const app = Vue.createApp({
-    data: function (){
-        return{
-            variant: 'valla',
+    data: function () {
+        return {
+            variant: 'vanilla',
             logo: 'images/logo.svg',
             vueLogo: 'images/vueLogo.svg',
-            cart: ['vanilla', 'orange', 'chocolate']
+            cart: ['vanilla', 'orange', 'chocolate'],
+            variants: [
+                {id: 1, name: 'Vanilla', price: 200},
+                {id: 2, name: 'Chocolate', price: 150},
+                {id: 3, name: 'Strawberry', price: 140},
+                {id: 4, name: 'Orange', price: 100},
+                {id: 1, name: 'Lemon', price: 50}
+            ]
         }
     }
 });
